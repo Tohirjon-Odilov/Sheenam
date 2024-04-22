bbf2117ca903d43e7f01004efc1be1776981cbc9
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-22 10:07:25 +0500

    add first files

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => 
+            Ok("Hello Mario, the princess is in another castle");
+    }
+}
