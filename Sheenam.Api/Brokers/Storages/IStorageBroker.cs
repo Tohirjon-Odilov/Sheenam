b7e58835c95e3e60febee5e0e1a8bd8b1649331e
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-28 21:25:54 +0500

    BROKERS: Insert guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,11 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+    }
+}
